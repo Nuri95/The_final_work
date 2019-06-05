536ba8a2127b9bcbb2726e8766258e5d5ce07ef4
Nuri <nurhotun95@gmail.com>
2019-06-05 17:39:15 +0500

    add one commit

--- python_telegram_bot.py
+++ python_telegram_bot.py
@@ -11,7 +11,8 @@ import requests
 token = '899979051:AAFn4mmftZNrTRLWTEm-BoVd7FJcye2WTPo'
 
 
-def answer(bot, update):
+
+def registration(bot, update):
 
     # for attr, value in update.__dict__.items():
     #     if attr is not None and value is not None:
@@ -27,10 +28,11 @@ def answer(bot, update):
                                 data=data )
     update.message.reply_text(
         'Hello {}'.format(update.message.from_user.first_name))
-    menu()
+    update.message.reply_text(menu())
 
 
-def menu():
+def menu(bot, update):
+    print(123)
     bot.send_chat_action(
         chat_id=update.message.chat_id,
         action=telegram.ChatAction.TYPING)  # Показать действие бота write пользователю
@@ -48,15 +50,180 @@ def menu():
         text="What do you want to do?",
         reply_markup=reply_markup
     )
+    return ACTION
+
+def action(bot, update):
+    if update.message.text == 'Categories':
+        bot.send_message(chat_id=update.message.chat_id,
+                         text=update.message.text)
+    elif update.message.text == 'Keywords':
+        bot.send_message(chat_id=update.message.chat_id,
+                         text=update.message.text)
+    return ANSWER
+
+
+def answer_check(bot, update):
+    bot.send_message(chat_id=update.message.chat_id,
+                     text='answer_check'
+                     )
+    return ANSWER
+
+
+def cancel(bot, update):
+    print('cancel')
+    # user = update.message.from_user
+    # logger.info("User %s canceled conversation",user.first_name)
+    update.message.reply_text('This conversation is over')
+    print('cancel_end')
+    return ConversationHandler.END
 
 
 def subscriptions(bot, update):
+    print('1')
     user_id = update.message.from_user.id
+    print('2')
+    # get_user = requests.get('http://127.0.0.1:8080/subscriptions/categories', user_id)
+    bot.send_message(chat_id=update.message.chat_id,
+                    parse_mode='markdown',
+                     text='[inline mention of a user](tg://user?id=899979051)'
+                     )
+                     # text='[okkk](https://vk.com)')
 
-    get_user = requests.get('http://127.0.0.1:8080/subscriptions/categories', user_id)
+
+CATEGORIES_LIST, CATEGORIES_MOVE = range(2)
+KEYWORDS_LIST, KEYWORDS_MOVE = range(2)
+def categories(bot,update):
+    user_id = str(update.message.from_user.id)
+    try:
+        a = requests.get('http://127.0.0.1:8080/categories').json()
+        print(a)
+        all_categories = 'All categories:\n'+ '\n'.join([str(v[0]) + ' ' + str(v[1]) for v in a])
+
+        bot.send_message(chat_id=update.message.chat_id,
+                         text=all_categories)
+    except e:
+        print(e)
+    try:
+        a = requests.get('http://127.0.0.1:8080/user/'+user_id+'/categories').json()
+
+        if a:
+            subscribed_categories = 'Your categories:\n'+ '\n'.join([str(v[0]) + ' ' + str(v[1]) for v in a])
+        else:
+            subscribed_categories = 'You are not subscribed to any category'
+        bot.send_message(chat_id=update.message.chat_id,
+                         text=subscribed_categories)
+    except e:
+        print(e)
+    return CATEGORIES_MOVE
+def move_category(bot,update):
+    print('move_CATEGORT')
+    text = update.message.text
+    user_id= str(update.message.from_user.id)
+    print('move_CATEGORT2')
+    if -1 != text.find('add'):
+        print('move_CATEGORT3')
+        text = text[text.find('add')+3:].strip()
+        print('user ' + user_id + ' add category ' +text)
+        try:
+            cat_id = int(text)
+            requests.post('http://127.0.0.1:8080/user/'+user_id+'/categories/add',data={"id":cat_id})
+        except e:
+            print(e)
+    elif -1 != text.find('remove'):
+        print('move_CATEGORT4')
+        text = text[text.find('remove')+6:].strip()
+        try:
+            print('move_CATEGORT5')
+            cat_id = int(text)
+            requests.post('http://127.0.0.1:8080/user/' + user_id+'/categories/remove', data={"id": cat_id})
+        except e:
+            print('move_CATEGORT6')
+            print(e)
+    elif (text == 'list'):
+        print('move_CATEGORT7')
+        categories(bot,update)
+
+    print('move_CATEGORT8')
+    #elif(text == 'cancel') :
+    #    return cancel()
+    return CATEGORIES_LIST
 
 
+def keywords(bot,update):
+    user_id = str(update.message.from_user.id)
+    try:
+        a = requests.get('http://127.0.0.1:8080/keywords').json()
+        print(a)
+        all_keywords = 'All keywords:\n'+ '\n'.join([str(v[0]) + ' ' + str(v[1]) for v in a])
 
+        bot.send_message(chat_id=update.message.chat_id,
+                         text=all_keywords)
+    except e:
+        print(e)
+    try:
+        a = requests.get('http://127.0.0.1:8080/user/'+user_id+'/keywords').json()
+
+        if a:
+            subscribed_keywords = 'Your keywords:\n'+ '\n'.join([str(v[0]) + ' ' + str(v[1]) for v in a])
+        else:
+            subscribed_keywords = 'You are not subscribed to any keywords'
+        bot.send_message(chat_id=update.message.chat_id,
+                         text=subscribed_keywords)
+    except e:
+        print(e)
+    return KEYWORDS_MOVE
+
+
+def move_keyword(bot,update):
+    print('move_KEYWORD1')
+    text = update.message.text
+    user_id= str(update.message.from_user.id)
+    print('move_KEYWORD2')
+    if -1 != text.find('add'):
+        print('move_KEYWORD3')
+        text = text[text.find('add')+3:].strip()
+        print('user ' + user_id + ' add keyword ' +text)
+        try:
+            cat_id = int(text)
+            requests.post('http://127.0.0.1:8080/user/'+user_id+'/keywords/add',data={"id":cat_id})
+        except e:
+            print(e)
+    elif -1 != text.find('remove'):
+        print('move_KEYWORD4')
+        text = text[text.find('remove')+6:].strip()
+        try:
+            print('move_KEYWORD5')
+            cat_id = int(text)
+            requests.post('http://127.0.0.1:8080/user/' + user_id+'/keywords/remove', data={"id": cat_id})
+        except e:
+            print('move_KEYWORD6')
+            print(e)
+    elif text == 'list':
+        print('move_KEYWORD7')
+        categories(bot,update)
+    print('move_KEYWORD8')
+    #elif(text == 'cancel') :
+    #    return cancel()
+
+    return KEYWORDS_LIST
+
+
+#
+# def news(bot, update):
+#     params={
+#             "pagesize": 5,
+#             "page":1,
+#             "category": "business",
+#             "country": "us",
+#             "keywor": "trump"
+#         }
+#     response = requests.get('http://127.0.0.1:8080/news', json=params)
+#     update.message.reply_text('\n'.join(response.json()))
+
+def news(bot, update):
+    user_id = update.message.from_user.id
+    response = requests.get('http://127.0.0.1:8080/user/'+user_id + '/news')
+    update.message.reply_text('\n'.join(response.json()))
 def main():
     bot = telegram.Bot(token)
     print(bot.get_me())
@@ -64,8 +231,29 @@ def main():
 
     dp = updater.dispatcher  # Этот класс отправляет все виды обновления в свои зарегистрированные обработчики
 
-    dp.add_handler(MessageHandler(Filters.text, answer))
+    category_handler = ConversationHandler(
+        entry_points=[CommandHandler('categories', categories)],
+        states={
+            CATEGORIES_MOVE: [MessageHandler(Filters.text, move_category)],
+            CATEGORIES_LIST: [MessageHandler(Filters.text, categories)],
+
+        },
+        fallbacks=[CommandHandler('cancel', cancel)]
+    )
+
+    keyword_handler = ConversationHandler(
+        entry_points=[CommandHandler('keywords', keywords)],
+        states={
+            KEYWORDS_MOVE: [MessageHandler(Filters.text, move_keyword)],
+            KEYWORDS_LIST: [MessageHandler(Filters.text, keywords)]
+        },
+        fallbacks=[CommandHandler('cancel', cancel)]
+    )
+    dp.add_handler(CommandHandler('register', registration))
+    dp.add_handler(category_handler)
+    dp.add_handler(keyword_handler)
     dp.add_handler(CommandHandler('menu', menu))
+    dp.add_handler(CommandHandler('news', news))
     dp.add_handler(CommandHandler('subscriptions', subscriptions))
     updater.start_polling()  # Запускает опрос обновлений от Telegram.
     updater.idle()  # Программу обновления останаливает
--- server_flask.py
+++ server_flask.py
@@ -1,17 +1,23 @@
-from flask import Flask, request, Response
+from flask import Flask, request, Response, jsonify
 import sqlite3 as lite
 import sys
 import pprint
+import json
+import requests
 
 app = Flask(__name__)
 
 
+def get_db():
+    con = lite.connect('../../bd_users.db')
+    cur = con.cursor()
+    return con,cur
 
 
 def add_user(id, first_name):
+    con = None
     try:
-        con = lite.connect('../../bd_users.db')
-        cur = con.cursor()
+        con, cur=get_db()
         cur.execute('''select * from users where id=(?)''',(id,))
         con.commit()
         data = cur.fetchall()
@@ -21,14 +27,15 @@ def add_user(id, first_name):
                         VALUES(?, ?)''', (id, first_name))
             con.commit()
             data = cur.fetchall()
+        result = True
 
     except Exception as e:
         print(e)
-        sys.exit(1)
+        result = False
     finally:
         if con is not None:
             con.close()
-    return data
+    return result
 
 
 def create_user():
@@ -43,37 +50,280 @@ def create_user():
                         status=400,
                         mimetype='application/json')
 
-    id = data['id']
-    first_name = data['first_name']
-    db = add_user(id, first_name)
-    return Response('{"Success": "ok"}',
+    if add_user(data['id'],data['first_name']):
+        return Response('{"Success": "ok"}',
                     status=200,
                     mimetype='application/json')
+    else:
+        return Response('{"Success": "0"}',
+                    status=500,
+                    mimetype='application/json')
 
-
-def subscriptions_categories():
-    try:
-        con = lite.connect('../../bd_users.db')
-        cur = con.cursor()
-        cur.execute('''select  c.categories from categories c  inner join users u on where id=(?)''', (id,))
-        con.commit()
-        data = cur.fetchall()
-
-    except Exception as e:
-        print(e)
-        sys.exit(1)
-    finally:
-        if con is not None:
-            con.close()
-    return data
 
 
 @app.route('/users', methods=['POST'])
 def users():
     return create_user()
 
-@app.route('/subscriptions/categories', methods=['GET'])
-def subscriptions():
-    return subscriptions_categories()
+@app.route('/categories', methods=['GET'])
+def categories():
+    con = None
+    try:
+        con, cur = get_db()
+        cur.execute('''select id, name from categories''')
+        con.commit()
+        data = cur.fetchall()
+        return Response(json.dumps(data),
+                        status=200,
+                        mimetype='application/json')
+
+    except Exception as e:
+        print(e)
+        return Response('{"Success": "0"}',
+                        status=500,
+                        mimetype='application/json')
+    finally:
+        if con is not None:
+            con.close()
+
+
+@app.route('/user/<id>/categories', methods=['GET'])
+def subscriptions_categories(id):
+    con = None
+    try:
+        con, cur = get_db()
+        cur.execute('''select c.id, c.name from categories c
+                        join subscriptions_category sc 
+                        on c.id = sc.categories_id where sc.user_id = ?''',(id,))
+        con.commit()
+        data = cur.fetchall()
+        return Response(json.dumps(data),
+                        status=200,
+                        mimetype='application/json')
+
+    except Exception as e:
+        print(e)
+        return Response('{"Success": "ololo"}',
+                        status=500,
+                        mimetype='application/json')
+    finally:
+        if con is not None:
+            con.close()
+
+
+@app.route('/user/<id>/categories/add', methods=['POST'])
+def add_category_to_user(id):
+    con = None
+    try:
+        print('request.form==', request.form)
+        data = request.form
+    except Exception as e:
+        print(e)
+        return Response({'status': 'ERROR'}, status=400)
+    if 'id' not in data:
+        return Response('{"status": "error","error":"Bad request"}',
+                        status=400,
+                        mimetype='application/json')
+    try:
+        con, cur = get_db()
+        cur.execute('''
+        INSERT INTO subscriptions_category(user_id, categories_id) values (?,?)
+        ''',(id,data['id']))
+        con.commit()
+        return Response('{"status": "OK"}', status=200)
+    except Exception as e:
+        print(e)
+        return Response({'status': 'ERROR'}, status=500)
+    finally:
+        if con is not None:
+            con.close()
+
+
+
+@app.route('/user/<id>/categories/remove', methods=['POST'])
+def remove_category_to_user(id):
+    con = None
+    try:
+        print('request.form==', request.form)
+        data = request.form
+    except Exception as e:
+        print(e)
+        return Response({'status': 'ERROR'}, status=400)
+    if 'id' not in data:
+        return Response('{"status": "error","error":"Bad request"}',
+                        status=400,
+                        mimetype='application/json')
+    try:
+        con, cur = get_db()
+        cur.execute('''
+            DELETE from subscriptions_category where user_id=? and categories_id=?
+            ''', (id, data['id']))
+        con.commit()
+        return Response('{"status": "OK"}', status=200)
+    except Exception as e:
+        print(e)
+        return Response({'status': 'ERROR'}, status=500)
+    finally:
+        if con is not None:
+            con.close()
+
+
+
+@app.route('/keywords', methods=['GET'])
+def keywords():
+    con = None
+    try:
+        con, cur = get_db()
+        cur.execute('''select id, name from keywords''')
+        con.commit()
+        data = cur.fetchall()
+        return Response(json.dumps(data),
+                        status=200,
+                        mimetype='application/json')
+
+    except Exception as e:
+        print(e)
+        return Response('{"Success": "0"}',
+                        status=500,
+                        mimetype='application/json')
+    finally:
+        if con is not None:
+            con.close()
+
+@app.route('/user/<id>/keywords', methods=['GET'])
+def subscriptions_keywords(id):
+    con = None
+    try:
+        con, cur = get_db()
+        cur.execute('''select c.id, c.name from keywords c
+                        join subscriptions_keywords sc 
+                        on c.id = sc.categories_id where sc.user_id = ?''',(id,))
+        con.commit()
+        data = cur.fetchall()
+        return Response(json.dumps(data),
+                        status=200,
+                        mimetype='application/json')
+
+    except Exception as e:
+        print(e)
+        return Response('{"Success": "ololo"}',
+                        status=500,
+                        mimetype='application/json')
+    finally:
+        if con is not None:
+            con.close()
+
+
+@app.route('/user/<id>/keywords/add', methods=['POST'])
+def add_keywords_to_user(id):
+    con = None
+    try:
+        print('request.form==', request.form)
+        data = request.form
+    except Exception as e:
+        print(e)
+        return Response({'status': 'ERROR'}, status=400)
+    if 'id' not in data:
+        return Response('{"status": "error","error":"Bad request"}',
+                        status=400,
+                        mimetype='application/json')
+    try:
+        con, cur = get_db()
+        cur.execute('''
+        INSERT INTO subscriptions_keywords(user_id, keywords_id) values (?,?)
+        ''',(id,data['id']))
+        con.commit()
+        return Response('{"status": "OK"}', status=200)
+    except Exception as e:
+        print(e)
+        return Response({'status': 'ERROR'}, status=500)
+    finally:
+        if con is not None:
+            con.close()
+
+
+
+@app.route('/user/<id>/keywords/remove', methods=['POST'])
+def remove_keywords_to_user(id):
+    con = None
+    try:
+        print('request.form==', request.form)
+        data = request.form
+    except Exception as e:
+        print(e)
+        return Response({'status': 'ERROR'}, status=400)
+    if 'id' not in data:
+        return Response('{"status": "error","error":"Bad request"}',
+                        status=400,
+                        mimetype='application/json')
+    try:
+        con, cur = get_db()
+        cur.execute('''
+            DELETE from subscriptions_keywords where user_id=? and keywords_id=?
+            ''', (id, data['id']))
+        con.commit()
+        return Response('{"status": "OK"}', status=200)
+    except Exception as e:
+        print(e)
+        return Response({'status': 'ERROR'}, status=500)
+    finally:
+        if con is not None:
+            con.close()
+
+@app.route('/user/<id>/news', methods=['GET'])
+def get_news(id):
+    # params = request.json
+    # params = {key: value for key, value in params.items() if value is not None}
+    con = None
+    try:
+        con, cur = get_db()
+        cur.execute('''
+                        select c.name from categoris c
+                        join subscriptions_category sc 
+                        on c.id = sc.keywords_id where sc.user_id = ?''',(id,)
+                    )
+        con.commit()
+        data = cur.fetchall()
+    except Exception as e:
+        print(e)
+        return Response({'status': 'ERROR'}, status=500)
+    finally:
+        if con is not None:
+            con.close()
+
+    params['apiKey'] = 'f7c60b043dc4473d8561f5e79a1b58f9'
+    try:
+        response = requests.get('https://newsapi.org/v2/top-headlines', params=params)
+
+        if response.status_code  != 200:
+            raise requests.ConnectionError(response.text)
+
+        response_json = response.json()
+
+        if response_json['status']  != 'ok':
+            raise requests.ConnectionError(response.text)
+
+    except requests.ConnectionError as c:
+        print(c)
+        exit()
+    except TimeoutError as t:
+        print('The waiting time is over.')
+        exit()
+    else:
+        # for article in response.json()['articles']:
+        #     print('title: ', article['title'])
+        #     print('publishedAt: ', article['publishedAt'])
+        #     print('url: ', article['url'])
+        list = [i['title'] for i in response.json()['articles']]
+        print(list, '/n')
+        # string=''
+        # for i in range(len(list)):
+        #     string = string+'\n'+list[i]
+        # print('\n'.join(list))
+        # return jsonify(**response_json)# **-распаковывает именнованные аргументы
+        return jsonify(list)
+
+    return 500, 'error---------------'
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080)
